f16c7445d7af04ac8405eebc60982c65d269aa97
Sana Ebadi <26750131+Sanaebadi97@users.noreply.github.com>
2018-07-20 08:07:55 -0400

    Add files via upload

---
+++ app/src/main/java/sanaebadi/info/ahwazdev/handler/CustomClickListener.java
@@ -0,0 +1,11 @@
+package sanaebadi.info.androidwordpress.handler;
+
+import android.view.View;
+
+/**
+ * Created by sanaebadi on 7/12/18.
+ */
+
+public interface CustomClickListener {
+  void itemClick(View view, int position);
+}
